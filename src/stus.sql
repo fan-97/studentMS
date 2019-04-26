d14a9e98f10569795a4dbaafebd0f5d41b0dffac
moon <moon@DESKTOP-1A456U2.lan>
2019-04-26 15:36:47 +0800

    数据库文件

--- src/stus.sql
+++ src/stus.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 08/04/2019 10:17:31
+ Date: 26/04/2019 15:16:41
 */
 
 SET NAMES utf8mb4;
@@ -32,6 +32,14 @@ CREATE TABLE `admin`  (
 INSERT INTO `admin` VALUES ('admin', 'admin');
 
 -- ----------------------------
+-- Table structure for con_test
+-- ----------------------------
+DROP TABLE IF EXISTS `con_test`;
+CREATE TABLE `con_test`  (
+  `a` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for stu
 -- ----------------------------
 DROP TABLE IF EXISTS `stu`;
@@ -43,12 +51,35 @@ CREATE TABLE `stu`  (
   `birthday` date NULL DEFAULT NULL,
   `hobby` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `info` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `grade` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`sid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of stu
 -- ----------------------------
-INSERT INTO `stu` VALUES (2, '刘京松', '男', '110', '2010-02-03', '游泳', '大家好，我是刘京东\r\n我来自达州\r\n我喜欢轮滑');
+INSERT INTO `stu` VALUES (1, '蔡徐坤', '女', '1888888888', '2019-04-04', '唱歌, 跳舞, 篮球', '我是练习时长两年半的蔡徐坤，我喜欢唱、跳、rap、篮球', '练习生001');
+INSERT INTO `stu` VALUES (2, 'cxk', '男', '11111111', '2019-04-12', '唱歌, 跳舞', '111', 'cxk1');
+INSERT INTO `stu` VALUES (5, '张三2', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (6, '张三3', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (7, '张三4', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (9, '张三5', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (10, '张三6', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (11, '张三7', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (12, '张三8', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (13, '张三9', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (14, '张三10', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (15, '张三11', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (16, '张三12', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (17, '张三13', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (18, '张三14', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (19, '张三15', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (20, '张三16', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (21, '张三17', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (22, '张三18', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (23, '张三19', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (24, '张三20', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (25, '张三21', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
+INSERT INTO `stu` VALUES (26, '张三21', '男', '155555555', '2018-11-16', '唱、跳、rap、篮球', '哈哈哈哈哈', '计科1641');
 
 SET FOREIGN_KEY_CHECKS = 1;
